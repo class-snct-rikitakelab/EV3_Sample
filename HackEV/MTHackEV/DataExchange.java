06fcf7ed1416b429b2721a0f7ddfe6a66fe8850a
Santtu <santeriq93@gmail.com>
2016-11-03 00:33:49 +0900

    Update DataExchange.java

--- HackEV/MTHackEV/DataExchange.java
+++ HackEV/MTHackEV/DataExchange.java
@@ -4,6 +4,7 @@ public class DataExchange extends Thread{
 	private boolean obstacleDetected = false,stop = false, followLeftSide = true;
 		
 	private float color = 0, distance = 0, rate=0, middleColor = 0;
+	float[] RGB = new float[3]; //Red = 0, Green = 1, Blue = 2
 	
 	private int time = 0;
 	
@@ -75,5 +76,40 @@ public class DataExchange extends Thread{
 	
 	public float GetMiddle(){
 		return middleColor;
+	}
+	
+	public void SetRGB(float[] newRGB){
+		/*
+		RGB[0] = newRGB[0];
+		RGB[1] = newRGB[1];
+		RGB[2] = newRGB[2];
+		*/
+		for(int i=0;i<3;i++){
+			RGB[i] = newRGB[i];
+		}
+	}
+	
+	public void SetRed(float newRed){
+		RGB[0] = newRed;
+	}
+	
+	public void SetGreen(float newGreen){
+		RGB[1] = newGreen;
+	}
+	
+	public void SetBlue(float newBlue){
+		RGB[2] = newBlue;
+	}
+	
+	public float GetRed(){
+		return RGB[0];
+	}
+	
+	public float GetGreen(){
+		return RGB[1];
+	}
+	
+	public float GetBlue(){
+		return RGB[2];
 	}
 }
